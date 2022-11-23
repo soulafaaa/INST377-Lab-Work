ade55566afc854552f0a7abca726fab0e683721b
soulafaaa <soulafahajali@gmail.com>
2022-11-23 15:34:45 -0500

    fixed minor errors

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -12,9 +12,9 @@
 */
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  const newmin = Math.ceil(min);
+  const newmax = Math.floor(max);
+  return Math.floor(Math.random() * (newmax - newmin + 1) + newmin); // The maximum is inclusive and the minimum is inclusive
 }
 
 function injectHTML(list) {
